d6b2f70cbadd962d2a3bb9cec1d94ea5143e81bc
Nate <nathaniel.leo.jones@gmail.com>
2021-02-10 15:37:09 -0500

    Error handling examples

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# node-babel-template
-This template creates a basic Node.js / Express.js API using babel. It also sets up nice defaults for eslint and prettier.
+# express-async-errors
+This repo goes along with my blog post on Express error handling using async / await.
 
-Check out the blog post explaining how to build this from scratch:
-https://medium.com/javascript-in-plain-english/a-minimal-node-js-express-babel-setup-part-1-6be7b3f3bb55
+The relevant code is in `src/app.js`.
+
+This repo also contains setup involving Babel, ESLint, Prettier and more -
+[see my template here](https://github.com/neightjones/node-babel-template)
+[or the medium post describing the setup, here.](https://medium.com/javascript-in-plain-english/a-minimal-node-js-express-babel-setup-part-1-6be7b3f3bb55)
--- src/app.js
+++ src/app.js
@@ -1,42 +1,99 @@
-import createError from 'http-errors';
+/* eslint-disable */
 import express from 'express';
-import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-
-import indexRouter from '#routes/index';
-import usersRouter from '#routes/users';
+import asyncHandler from 'express-async-handler';
 
 const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/**
+ * For reference: Express error handling guide (using v4, here):
+ * https://expressjs.com/en/guide/error-handling.html
+ */
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
+/**
+ * I'm an async function to simulate async network
+ * or database call. I throw an error after a second!
+ */
+const asyncFn = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error('Async Fn error!'));
+    }, 1000);
+  });
+};
+
+/**
+ * I throw an error in simple, synchronous code.
+ * Express will automatically pass this error to our
+ * error handler middleware (down at bottom).
+ */
+app.get('/sync-test', (req, res) => {
+  throw new Error('Error from synchronous code!');
 });
 
-// error handler
-/* eslint-disable no-unused-vars */
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+/**
+ * I'm an async route handler (see my 'async' keyword).
+ * When I try awaiting asyncFn defined above, what'll happen
+ * with that error? Express WON'T automatically pass the error
+ * to our error handler defined below! :0
+ * This leads to the fateful UnhandledPromiseRejection.
+ */
+app.get('/async-test-1', async (req, res) => {
+  await asyncFn();
+  res.json({ well: `We're not going to reach this line.` });
+});
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+/**
+ * This is like the async test above, but now we're using the
+ * try...catch syntax to catch the async error. Then, we
+ * can call 'next' to forward the error to our error handler.
+ * We need to do this for async errors!
+ */
+app.get('/async-test-2', async (req, res, next) => {
+  try {
+    await asyncFn();
+    res.json({ well: `We're not going to reach this line, either.` });
+  } catch (e) {
+    // This time, we'll catch the error and pass it along
+    // to our error handler, below.
+    next(e);
+  }
+});
+
+/**
+ * The last code works pretty well, but do you really want
+ * to wrap every route handler in a try...catch?
+ * The express-async-handler package takes care of that for us!
+ * So this code works equally as well as the above.
+ * Look how we wrap our handler -
+ */
+app.get(
+  '/async-test-3',
+  asyncHandler(async (req, res) => {
+    await asyncFn();
+    res.json({ well: `We're *still* not going to reach this line.` });
+  }),
+);
+
+/**
+ * I'm the custom error handler - notice my 4 params.
+ * Errors in synchronous code are passed to me automatically
+ * by Express, but in asynchronous code, we need to be more careful!
+ * In real life, we probably have special error handlers above this
+ * one, e.g. to catch 404's, etc.
+ */
+app.use((err, req, res, next) => {
+  // Simple error handling here... in real life we might
+  // want to be more specific
+  console.log(`I'm the error handler. '${err.message}'`);
+  res.status(500);
+  res.json({ error: err.message });
 });
 
 export default app;
