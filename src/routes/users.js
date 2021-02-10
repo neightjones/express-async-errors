99fd933917ec0f5ee217511c265dec6bdcc6400d
Nate <nathaniel.leo.jones@gmail.com>
2021-02-10 11:35:18 -0500

    initial commit - copying template repo

---
+++ src/routes/users.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', (req, res) => {
+  res.send('respond with a resource');
+});
+
+export default router;
